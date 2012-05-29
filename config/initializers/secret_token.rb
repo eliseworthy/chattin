321e93e645745a01fb4bdba484a1c0b792d911eb
Elise Worthy <eworthy@bluebox.net>
2012-05-29 17:14:30 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chattin::Application.config.secret_token = 'ba4c150ab0c648b03f0bedf932a74e3f7694592373a15b9c1f1d845fa2121c031ca9f0161a7ab28424d87441fb5e7e56e20ebcfeb4d11ab1c35a126092e52f38'
