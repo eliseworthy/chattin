befaa78b1863b906a71e51b5fbbf7c9a5031099d
Elise Worthy <eworthy@bluebox.net>
2012-05-29 18:11:31 -0400

    removed unecessary message files

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,18 +2,9 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
-  def new
-    @message = Message.new
-  end
+
   def create
-    @message = Message.new(params[:message])
-    if @message.save
-      redirect_to messages_path
-    else
-      flash[:notice] = "boom"
-      redirect_to action: :new
-      # render template: "messages/new",
-      #         notice: "You suck."
-    end
+    @message = Message.create!(params[:message])
+    redirect_to messages_path
   end
 end
