c9c7083cf79045214ecec817625f92f54dd4c4a6
Elise Worthy <eworthy@bluebox.net>
2012-05-29 17:59:55 -0400

    see all the messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,18 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+  def new
+    @message = Message.new
+  end
+  def create
+    @message = Message.new(params[:message])
+    if @message.save
+      redirect_to messages_path
+    else
+      flash[:notice] = "boom"
+      redirect_to action: :new
+      # render template: "messages/new",
+      #         notice: "You suck."
+    end
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ActiveRecord::Base
   attr_accessible :body
+  validates_presence_of :body
 end
